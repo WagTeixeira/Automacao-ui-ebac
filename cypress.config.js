5333078243fd7f0e1e5282eabb9e9360a08fe340
Wagner Teixeira <wagalencar1998@outlook.com>
2023-06-08 14:31:57 -0300

    primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  
+  projectId: 'hxxzy8',
+  e2e: {  
+      "baseUrl":'http://lojaebac.ebaconline.art.br'   
+  },
+});
+
---
+++ cypress/support/pages/login.page.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+class LoginPage {
+    get #user() {return cy.get("#user_login")}
+    get #pass() {return cy.get("#user_pass")}
+    get #login() {return cy.get("#wp-submit")}
+
+    login (user, pass){
+        this.#user.type(user)
+        this.#pass.type(pass)
+        this.#login.click()
+        
+    }
+}
+
+module.exports = new LoginPage()
+/*
+    #user_login
+    #user_pass
+    #wp-submit
+ */
